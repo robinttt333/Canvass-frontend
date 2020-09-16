20b8fec5f5a77e310981d2121eeaf0450b7a743f
robinttt333 <robinttt333@gmail.com>
2020-09-16 14:44:41 +0530

    cookie+auth token setup done

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,12 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { createHttpLink, ApolloClient, InMemoryCache } from "@apollo/client";
+
+const link = createHttpLink({
+	uri: "http://127.0.0.1:4000/graphql",
+	credentials: "include",
+});
 
 const client = new ApolloClient({
-	uri: "http://127.0.0.1:4000",
+	link,
 	cache: new InMemoryCache(),
 });
 
